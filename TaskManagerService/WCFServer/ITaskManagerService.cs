2d9bbaaaee544c7bea7f3099c1e20e4460cf1f82
Paul-work <pzaloznyj@gmail.com>
2012-09-26 16:04:45 +0300

    added client wcf

---
+++ TaskManagerService/WCFServer/ITaskManagerService.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+using EntitiesLibrary;
+
+namespace TaskManagerService.WCFServer
+{
+    [ServiceContract]
+    public interface ITaskManagerService
+    {
+        [OperationContract]
+        ITask AddTask(ITask task);
+    }
+}
