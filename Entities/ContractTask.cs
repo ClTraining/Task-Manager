9bdd88ba14e3853dc78f1d2b631a7d4af98a458e
Paul Zaloznyj <pzaloznyj@gmail.com>
2012-09-26 09:13:54 +0300

    removed old refs

--- Entities/ContractTask.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EntitiesLibrary
-{
-    public class ContractTask
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- Entities/ServiceTask.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EntitiesLibrary
-{
-    public class ServiceTask
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
