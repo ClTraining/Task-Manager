c1561b5e47fd75b4d0365ba4a4c6d07ff9511da1
Paul-work <pzaloznyj@gmail.com>
2012-09-26 12:02:38 +0300

    tests for todolist

--- TaskManagerService/TaskManager/ToDoList.cs
+++ TaskManagerService/TaskManager/ToDoList.cs
@@ -15,5 +15,13 @@ namespace TaskManagerService.TaskManager
 
     public class ToDoListTests
     {
+        private ITask incomingTask = new ContractTask {Id = 0, Name = "Buy cheese"};
+        private ITask expectedTask = new ServiceTask {Id = 1, Name = "Buy cheese"};
+
+        [Fact]
+        public void todolist_asks_factory_for_new_task_and_saves_list()
+        {
+            
+        }
     }
 }
