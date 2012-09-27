bf38b74a8359b9a85c703e898374ccf2075b2d02
hgordeyeva <heleng@mak007.compatibl.com>
2012-09-27 17:35:17 +0300

    Added ninject initialization

--- TaskConsoleClient/UI/Program.cs
+++ TaskConsoleClient/UI/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.IO;
+using Ninject;
+using Ninject.Modules;
 using TaskConsoleClient.Manager;
-using TaskManagerHost.WCFServer;
 
 namespace TaskConsoleClient.UI
 {
@@ -10,12 +10,22 @@ namespace TaskConsoleClient.UI
 
         static void Main()
         {
-            var helper = new ConsoleHelper(new CommandManager());
+            var module = new TaskManagerModule();
+            var kernel = new StandardKernel(module);
+            var consoleHelper = kernel.Get<IConsoleHelper>();
             string s;
             while ((s = Console.ReadLine()) != null)
             {
-                helper.Parse(s);
+                consoleHelper.Parse(s);
             }
+        }
+    }
+    public class TaskManagerModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IConsoleHelper>().To<ConsoleHelper>();
+            Bind<ICommandManager>().To<CommandManager>();
         }
     }
 }
