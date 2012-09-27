6d9b4a3ef7dbc809835621a3fc568f785055ee35
hgordeyeva <heleng@mak007.compatibl.com>
2012-09-27 18:19:01 +0300

    Some fixes

--- TaskConsoleClient/UI/Program.cs
+++ TaskConsoleClient/UI/Program.cs
@@ -1,49 +1,15 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 ﻿using TaskConsoleClient.Manager;
 ﻿using System;
-<<<<<<< HEAD
-=======
 using System.ServiceModel;
 using EntitiesLibrary;
 using TaskConsoleClient.Manager;
 using TaskManagerHost.WCFServer;
->>>>>>> updated
 
-=======
-﻿using System;
-using Ninject;
-using Ninject.Modules;
-using TaskConsoleClient.Manager;
->>>>>>> 4dd826778d6c0aa0bb31c5c3e59682e9053b13d9
-
-namespace TaskConsoleClient.UI
-{
-    static class Program
-    {
-        static void Main()
-        {
-<<<<<<< HEAD
-            var client = new CommandManager();
-            while (true)
-            {
-                client.Run();
-            }
-        }
-    }
-}
-=======
 ﻿namespace TaskConsoleClient.UI
 {
     {
         static void Main()
         {
-<<<<<<< HEAD
-            var task = new ContractTask();
-            var factory = new ChannelFactory<ITaskManagerService>(new NetTcpBinding(), "net.tcp://localhost:44444");
-            var client = factory.CreateChannel();
-            client.AddTask(task);
-=======
             var module = new TaskManagerModule();
             var kernel = new StandardKernel(module);
             var consoleHelper = kernel.Get<IConsoleHelper>();
@@ -60,17 +26,7 @@ namespace TaskConsoleClient.UI
         {
             Bind<IConsoleHelper>().To<ConsoleHelper>();
             Bind<ICommandManager>().To<CommandManager>();
->>>>>>> 4dd826778d6c0aa0bb31c5c3e59682e9053b13d9
         }
     }
 }
->>>>>>> added wcf client
-=======
-            //var task = new ConsoleHelper().Parse(Console.ReadLine())
-            //var factory = new ChannelFactory<ITaskManagerService>(new NetTcpBinding(), "net.tcp://localhost:44444");
-            //var client = factory.CreateChannel();
-            //client.AddTask(task);
-        }
-    }
-}
->>>>>>> updated
+
