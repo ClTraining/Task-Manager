b5886647fa1cbe63ad37ba55d9fe5f97ff335e04
hgordeyeva <heleng@mak007.compatibl.com>
2012-09-27 17:28:55 +0300

    Added basic main implementation

--- TaskConsoleClient/UI/Program.cs
+++ TaskConsoleClient/UI/Program.cs
@@ -1,13 +1,21 @@
-﻿using TaskConsoleClient.Manager;
+﻿using System;
+using System.IO;
+using TaskConsoleClient.Manager;
 using TaskManagerHost.WCFServer;
 
 namespace TaskConsoleClient.UI
 {
     static class Program
     {
+
         static void Main()
         {
-            
+            var helper = new ConsoleHelper(new CommandManager());
+            string s;
+            while ((s = Console.ReadLine()) != null)
+            {
+                helper.Parse(s);
+            }
         }
     }
 }
