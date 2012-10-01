d1b4bb2a42556ccf87b5a8768b2a2b19e785695e
hgordeyeva <heleng@mak007.compatibl.com>
2012-10-01 11:46:43 +0300

    Added tests for TaskMapper

--- TaskManagerService/TaskManager/TaskMapper.cs
+++ TaskManagerService/TaskManager/TaskMapper.cs
@@ -1,13 +1,15 @@
 ﻿#region Using
 
 using EntitiesLibrary;
+using FluentAssertions;
+using Xunit;
 
 #endregion
 
 
 namespace TaskManagerHost.TaskManager
 {
-    class TaskMapper:ITaskMapper
+    public class TaskMapper:ITaskMapper
     {
         public ServiceTask ConvertToService(ContractTask task)
         {
@@ -19,6 +21,33 @@ namespace TaskManagerHost.TaskManager
         {
             var newTask = new ContractTask { Id = task.Id, Name = task.Name, IsCompleted = task.IsCompleted };
             return newTask;
+        }
+    }
+
+    public class TaskMapperTests
+    {
+        private ServiceTask serviceTask = new ServiceTask(){Id = 10, Name = "service", IsCompleted = true};
+        private ContractTask contractTask = new ContractTask(){Id = 11, Name = "contract", IsCompleted = false};
+        private TaskMapper taskMapper = new TaskMapper();
+
+        [Fact]
+        public void should_convert_service_to_contract()
+        {
+            var result = taskMapper.ConvertToContract(serviceTask);
+            result.Should().BeOfType<ContractTask>();
+            result.Name.Should().Be(serviceTask.Name);
+            result.Id.Should().Be(serviceTask.Id);
+            result.IsCompleted.Should().Be(serviceTask.IsCompleted);
+        }
+
+        [Fact]
+        public void should_convert_contract_to_service()
+        {
+            var result = taskMapper.ConvertToService(contractTask);
+            result.Should().BeOfType<ServiceTask>();
+            result.Name.Should().Be(contractTask.Name);
+            result.Id.Should().Be(contractTask.Id);
+            result.IsCompleted.Should().Be(contractTask.IsCompleted);
         }
     }
 }
