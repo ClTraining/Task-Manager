77beb14dbfdf0cbe4daa634f1c7cc609962fd47f
Paul-work <pzaloznyj@gmail.com>
2012-10-03 22:30:54 +0300

    updated

---
+++ TaskManagerService/WCFService/TaskNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TaskManagerHost.WCFService
+{
+    public class TaskNotFoundException : Exception
+    {
+        public TaskNotFoundException(int id) : base(id.ToString()) { }
+        public TaskNotFoundException(string message) : base(message) { }
+    }
+}
