741e58bdba430963a993a95d11b96225738ca0e6
Paul-work <pzaloznyj@gmail.com>
2012-10-10 11:09:18 +0300

    fixed tests on taskmapper

--- TaskManagerServiceLibrary/TaskManager/ITaskMapper.cs
+++ TaskManagerServiceLibrary/TaskManager/ITaskMapper.cs
@@ -4,7 +4,6 @@ namespace TaskManagerServiceLibrary.TaskManager
 {
     public interface ITaskMapper
     {
-        ServiceTask ConvertToService(ContractTask task);
         ContractTask ConvertToContract(ServiceTask task);
     }
 }
