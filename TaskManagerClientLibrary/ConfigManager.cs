b10f8947b6b0fa6b0f9fdd162eaa74e1280636b5
kravasb <kravasb@gmail.com>
2012-10-12 17:33:20 +0300

    Greeting added

---
+++ TaskManagerClientLibrary/ConfigManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerClientLibrary
+{
+    public class ConfigurationManager
+    {
+        public string GetAddress()
+        {
+            var config = System.Configuration.ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var address = config.AppSettings.Settings["connectionAddress"].Value;
+            return address;
+        }
+    }
+}
---
+++ TaskManagerClientLibrary/UserNotifier.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerClientLibrary
+{
+    public class UserNotifier
+    {
+        private readonly string address;
+
+        public UserNotifier(string address)
+        {
+            this.address = address;
+        }
+        
+        public void Greet()
+        {
+            Console.WriteLine("Hello " + Environment.UserName);
+            Console.WriteLine("\nServer address is: " + address.Split(new[] { '/', ':' })[3] );
+            Console.WriteLine("Type \'?\' to see available commands");
+        }
+    }
+}
