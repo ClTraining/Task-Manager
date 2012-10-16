820f06eddb26b80dbb7caa953ecf25fcf7c46710
Paul-work <pzaloznyj@gmail.com>
2012-10-16 11:12:46 +0300

    added Help class

---
+++ TaskManagerClientLibrary/ConcreteHandlers/Help.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConnectToWcf;
+
+namespace TaskManagerClientLibrary.ConcreteHandlers
+{
+    class Help : Command<string>
+    {
+        public Help(Type derived) : base(typeof(Help)) { }
+
+        protected override void ExecuteWithGenericInput(string input)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
